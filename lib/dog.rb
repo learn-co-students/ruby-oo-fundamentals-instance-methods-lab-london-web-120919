3e99b14af09840483cad2b5ec346dbf35747bb7b
santiago molinari <santomolinari@gmail.com>
2019-12-10 10:39:50 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+    def bark               #instance method bark
+        puts "Whoof!"
+    end
+    
+    def sit                 #instance method sitting
+        puts "The Dog is sitting"
+    end
+end
